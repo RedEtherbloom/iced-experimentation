ab662ab2708510c3278983eab6f6a670c00651c6
RedEtherbloom <etherbloom@mailbox.org>
2026-08-23 21:58:51 +0200

    feat: Limit text to one line

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use iced::{
-    Alignment, Element, Fill, Subscription, border,
-    widget::{column, container, scrollable, space, text, theme},
+    advanced::text::Wrapping,
+    Alignment, Element, Fill, Font, Pixels, Subscription, border,
+    font::Weight,
+    widget::{column, container, keyed_column, row, scrollable, text, text::LineHeight, theme},
 };
 
 #[derive(Debug, Clone)]
@@ -19,27 +21,44 @@ impl RssView {
     }
 
     fn view(&self) -> Element<'_, Message> {
+        const DEFAULT_ICED_FONT_SIZE: f32 = 16.0;
+        let height_single_line = LineHeight::default().to_absolute(Pixels(DEFAULT_ICED_FONT_SIZE));
         column![
             text("This is the ControlBar placeholder"),
             container(
-                scrollable(column![
-                    "Test Entry 1",
-                    "Test Entry 2",
-                    space().height(400),
-                    "Test Entry 3",
-                ])
+                scrollable(
+                    keyed_column((1..=100).map(|i| {
+                        (
+                            uuid::Uuid::now_v7(),
+                            row![
+                                container(column![
+                                    text!("Element number {i}").font(Font {
+                                        weight: Weight::Semibold,
+                                        ..Default::default()
+                                    }),
+                                    text!("This is a placeholder! This is a placeholder! This is a placeholder! This is a placeholder! This is a placeholder! This is a placeholder! This is a placeholder! This is a placeholder!")
+                                        .font(Font {
+                                            weight: Weight::ExtraLight,
+                                            ..Default::default()
+                                        }).height(height_single_line)
+                                    .width(Fill).wrapping(Wrapping::WordOrGlyph)
+                                ])
+                                .style(style_only_border_box)
+                                .padding(8)
+                                .width(Fill)
+                            ]
+                            .into(),
+                        )
+                    }))
+                    .spacing(4)
+                    .padding(4)
+                )
                 .height(Fill)
                 .width(Fill)
+                .spacing(8)
             )
             .align_x(Alignment::End)
-            .style(|theme: &theme::Theme| container::Style {
-                border: border::Border {
-                    width: 1.0,
-                    radius: 5.0.into(),
-                    color: theme.extended_palette().background.weak.color,
-                },
-                ..container::Style::default()
-            })
+            .style(style_only_border_box)
         ]
         .align_x(Alignment::Center)
         .width(Fill)
@@ -53,11 +72,16 @@ impl RssView {
     }
 }
 
-#[allow(dead_code)]
-struct ControlBar {}
-
-#[allow(dead_code)]
-struct RssScrollbar {}
+fn style_only_border_box(theme: &theme::Theme) -> container::Style {
+    container::Style {
+        border: border::Border {
+            width: 1.0,
+            radius: 5.0.into(),
+            color: theme.extended_palette().background.weak.color,
+        },
+        ..container::Style::default()
+    }
+}
 
 fn main() -> Result<(), iced::Error> {
     println!("Hello, world!");
